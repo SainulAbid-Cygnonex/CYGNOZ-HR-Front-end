f7b2de0fde257345e2c0fd6d3c093f7ac9b9db39
Lavanya-A-K <lavanya@codzoc.com>
2024-06-26 17:16:19 +0530

    added a time sheet component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Router, Route, Routes } from 'react-router-dom';
 import Login from './components/authentication/Login';
 import Header from './components/Header';
 import ProductManagement from './pages/ProductManagement';
+import TimeSheet from './components/TimeSheet';
 
 function App() {
  
@@ -12,6 +13,7 @@ function App() {
     <Header />
     <Routes>
       <Route path="/"  element={<Login/>} />
+      <Route path="/timesheet"  element={<TimeSheet/>} />
       <Route path="/products"  element={<ProductManagement/>} />
       {/* Add routes for other components like Dashboard here */}
     </Routes>
---
+++ src/components/TimeSheet.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+
+import {
+    
+    InputLabel,
+    FormControl,
+    Select,
+    MenuItem,
+    Button
+  } from '@mui/material';
+
+
+function TimeSheet() {
+
+    const [technicalType, setTechnicalType] = useState('');
+
+    const handleTypeChange = (event) => {
+        setTechnicalType(event.target.value);
+      };
+
+  return (
+    <>
+      <div className='text-3xl font-bold text-center mt-5'>Time Sheet</div>
+      <div >
+        <h3 className='text-lg my-5 mx-5'>
+          Welcome <span className='text-blue-500 font-bold'>Employee</span> ,
+        </h3>
+      </div>
+      <div className="mx-5">
+       
+        <div className='flex justify-between gap-10'>
+        <FormControl variant="outlined" fullWidth style={{width:"200px"}}>
+        <InputLabel id="user-type-label">Technical Type</InputLabel>
+            <Select
+                labelId="technical-type-label"
+                id="technical-type"
+                value={technicalType} 
+                onChange={handleTypeChange}
+                label="technical-type"
+                required
+              >
+                <MenuItem value="technical">Technical</MenuItem>
+                <MenuItem value="non-technical">Non-Technical</MenuItem>
+              </Select>
+              </FormControl>
+              <Button variant='contained'>Add +</Button>
+        </div>
+
+        <div className='table-timeheet mt-10'>
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">#</th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product Name</th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Hours/day</th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <tr>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">SEWNEX</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  <input type="number" min={0} max={8} className="border border-gray-300 rounded p-1" />
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Billbizz</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  <input type="number" min={0} max={8} className="border border-gray-300 rounded p-1" />
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"></td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className='flex justify-center mt-5'>
+         <Button variant='contained'>Submit</Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TimeSheet;
